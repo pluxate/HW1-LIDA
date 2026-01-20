c06859b23a36b3da88790d630db21da85d5f392d
Zach Allen <zach12399@gmail.com>
2026-01-20 14:44:19 -0600

    updated for templates + basics

--- HW1-LIDA/array_list.h
+++ HW1-LIDA/array_list.h
@@ -1,8 +1,15 @@
 #pragma once
 
-class array_list {
+#include <cstddef>
+#include <iostream>
+
+template <typename T> class array_list {
   public:
-    array_list();
+    array_list() { std::cout << "array list created" << '\n'; }
+    ~array_list() { std::cout << "array list destroyed" << '\n'; }
 
   private:
+    T m_items[1];
+    size_t m_size = 1;
+    size_t m_count = 0;
 };
