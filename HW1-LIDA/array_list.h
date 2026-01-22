ecfa9ead12d6edf66ed00eb858534e4c526b587e
zachakaquack <zach12399@gmail.com>
2026-01-21 19:06:11 -0600

    even better funciton name

--- HW1-LIDA/array_list.h
+++ HW1-LIDA/array_list.h
@@ -13,7 +13,7 @@ template <typename T> class array_list {
 
     size_t size() { return m_size; }
     size_t count() { return m_count; }
-    T pleasegetthisspecificthinghereatindexofx(size_t x) { return m_items[x]; }
+    T pleasegetthisspecificthinghereatindexof(size_t x) { return m_items[x]; }
 
     T *begin() { return &m_items[0]; }
     T *end() { return (1 + &m_items[m_count - 1]); }
@@ -22,7 +22,7 @@ template <typename T> class array_list {
         size_t i = 0;
         for (; i < arraylist.count(); i++) {
             out << i << ":"
-                << arraylist.pleasegetthisspecificthinghereatindexofx(i) << " ";
+                << arraylist.pleasegetthisspecificthinghereatindexof(i) << " ";
         }
         for (; i < arraylist.size(); i++) {
             out << i << ":" << "nil" << " ";
